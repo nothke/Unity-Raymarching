4849774c3894569fb313d326ac399b28561a5ac3
Ivan Notaros <nothke@gmail.com>
2016-09-03 02:46:02 +0300

    Flyable camera, quad in front of cam

---
+++ Assets/Scripts/Flycam.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flycam : MonoBehaviour
+{
+
+    public float speed;
+
+    bool slow;
+    bool fast;
+
+    void Start()
+    {
+
+    }
+
+    float GetAxis(KeyCode keyNegative, KeyCode keyPositive)
+    {
+        return Input.GetKey(keyNegative) ? -1 : Input.GetKey(keyPositive) ? 1 : 0;
+    }
+
+    void Update()
+    {
+        float horizontal = GetAxis(KeyCode.A, KeyCode.D);
+        float forward = GetAxis(KeyCode.S, KeyCode.W);
+        float vertical = GetAxis(KeyCode.C, KeyCode.X);
+        
+
+
+        Vector3 input = new Vector3(horizontal, vertical, forward);
+
+
+        transform.Translate(input * Time.deltaTime, Space.Self);
+
+        transform.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0, Space.Self);
+    }
+}
